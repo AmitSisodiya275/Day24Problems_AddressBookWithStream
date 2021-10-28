e0f041e1ed1bdca782341311ea5ff6f0e0470777
AmitSisodiya275 <amitsisodiya275@gmail.com>
2021-10-28 20:53:37 +0530

    Added: a new welcome file which can print welcome message on the console.

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+package com.bridgelab.addressbook.stream;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to day 24 Problems of Address Book using Stream.");
+	}
+
+}
